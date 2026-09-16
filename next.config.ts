6a60a8165c98b0de68fd8b0de7437c25d29d8dcd
IgnacioDiazValdez <nachodiazvaldez@gmail.com>
2026-09-16 12:29:36 -0300

    segundo parche de seguridad

--- next.config.ts
+++ next.config.ts
@@ -33,7 +33,7 @@ const nextConfig: NextConfig = {
         },
         {
           key: "Content-Security-Policy",
-          value: "default-src 'self'; style-src 'self' 'unsafe-inline'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; img-src 'self' https://*.supabase.co; frame-src https://*.supabase.co; connect-src 'self' https://*.supabase.co https://api.groq.com https://dolarapi.com; frame-ancestors 'none'",
+          value: "upgrade-insecure-requests; default-src 'self'; base-uri 'self'; object-src 'none'; form-action 'self'; style-src 'self' 'unsafe-inline'; script-src 'self' 'unsafe-inline'; img-src 'self' blob: data: https://*.supabase.co; frame-src https://*.supabase.co; connect-src 'self' https://*.supabase.co https://api.groq.com https://dolarapi.com; frame-ancestors 'none'",
         },
       ],
     },
